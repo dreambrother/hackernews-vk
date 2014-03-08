606a270ca06e1178e554cef8a39bdc9b78bab825
dreambrother <nikshmakov@gmail.com>
2014-03-09 00:57:18 +0400

    main method skeleton

--- src/main/java/com/github/dreambrother/hackernews/Main.java
+++ src/main/java/com/github/dreambrother/hackernews/Main.java
@@ -1,7 +1,45 @@
 package com.github.dreambrother.hackernews;
 
+import com.github.dreambrother.hackernews.io.StdIOImpl;
+import com.github.dreambrother.hackernews.wd.WatchdogPingListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        log.info("Start hackernews-vk publicator application");
+
+        Main app = new Main();
+        app.startWdListener();
+        app.registerShutdownHook();
+        app.startApplication();
+    }
+
+    private void startApplication() {
+        // TODO: stub implementation
+        while (true) {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startWdListener() {
+        WatchdogPingListener listener = new WatchdogPingListener();
+        listener.setStdIO(new StdIOImpl());
+        listener.start();
+    }
+
+    private void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                log.info("Just before shutdown the programm");
+            }
+        }));
     }
 }
