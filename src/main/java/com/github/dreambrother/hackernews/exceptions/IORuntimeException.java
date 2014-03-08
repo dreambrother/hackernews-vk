0137ded412475a08b3db3a60567adfce16dcd324
dreambrother <nikshmakov@gmail.com>
2014-03-09 00:42:53 +0400

    add StdIOImpl

---
+++ src/main/java/com/github/dreambrother/hackernews/exceptions/IORuntimeException.java
@@ -0,0 +1,8 @@
+package com.github.dreambrother.hackernews.exceptions;
+
+public class IORuntimeException extends RuntimeException {
+
+    public IORuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
