57d4f2ab51c9edcaf59e648b1f29789a89f3a639
dreambrother <nikshmakov@gmail.com>
2014-03-11 23:19:33 +0400

    add test for 500

--- src/test/java/com/github/dreambrother/hackernews/http/PingServerIntTest.java
+++ src/test/java/com/github/dreambrother/hackernews/http/PingServerIntTest.java
@@ -1,6 +1,8 @@
 package com.github.dreambrother.hackernews.http;
 
 import com.github.dreambrother.hackernews.exceptions.RuntimeIOException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.fluent.Request;
 import org.junit.After;
 import org.junit.Before;
@@ -11,6 +13,8 @@ import org.mockito.MockitoAnnotations;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -46,11 +50,28 @@ public class PingServerIntTest {
         verify(handlerMock).handle(relativeUrl);
     }
 
+    @Test
+    public void shouldHandleError() {
+        when(handlerMock.handle(anyString())).thenThrow(RuntimeException.class);
+        expectErrorCode(500);
+    }
+
     private String httpGet(String relativeUrl) {
         try {
             return Request.Get("http://localhost:10000" + relativeUrl).execute().returnContent().asString();
         } catch (IOException e) {
             throw new RuntimeIOException(e);
+        }
+    }
+
+    private void expectErrorCode(int code) {
+        try {
+            Request.Get("http://localhost:10000").execute().returnContent().asString();
+            fail();
+        } catch (HttpResponseException ex) {
+            assertEquals(code, ex.getStatusCode());
+        } catch (Exception ignored) {
+            fail();
         }
     }
 }
