17113bf5e124948eb317edf1721c233903da57e6
dreambrother <nikshmakov@gmail.com>
2014-03-15 21:30:58 +0400

    add scheduled publicator

---
+++ src/main/java/com/github/dreambrother/hackernews/worker/ScheduledPublicator.java
@@ -0,0 +1,30 @@
+package com.github.dreambrother.hackernews.worker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledPublicator {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledPublicator.class);
+
+    private ScheduledExecutorService scheduledExecutorService;
+
+    public void start(Publicator publicator, int delayMillis) {
+        log.info("Start scheduled publicator");
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleWithFixedDelay(publicator, 0, delayMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public void shutdown() {
+        if (scheduledExecutorService != null) {
+            log.info("Shutdown scheduled publicator");
+            scheduledExecutorService.shutdown();
+        } else {
+            log.warn("Attempt to shutdown scheduled publicator without starting it!");
+        }
+    }
+}
