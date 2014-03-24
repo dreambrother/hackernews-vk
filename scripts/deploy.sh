f6b42ee6a4bda58173e38162ca65d5e303ece993
dreambrother <nikshmakov@gmail.com>
2014-03-25 00:41:20 +0400

    remove remote lib dir before deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 echo "Build project"
 ./scripts/make.sh
+echo "Remove old app lib"
+ssh hackernewsvk 'rm -rf app/lib'
 echo "Copy app to server"
 scp -r target/app hackernewsvk:
 echo "Connect to remote server"
