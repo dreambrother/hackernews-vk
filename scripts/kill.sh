ced13bf37ef42e58cf0dadf2c6fc8f3df9fc3033
dreambrother <nikshmakov@gmail.com>
2014-03-25 00:02:32 +0400

    restart wd and app after deploy

---
+++ scripts/kill.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+getAppPid() {
+    appPid=$(jps | grep HackernewsVkApp | awk '{print $1}')    
+}
+
+getWdPid() {
+    wdPid=$(pgrep watchdog.sh)
+}
+
+killSafely() {
+    pid=$1
+    if [ -n "$pid" ]; then
+        echo "Process is running, pid=$pid, try to terminate"
+        kill $pid
+        dead=false
+    else
+        echo "Process is dead"
+        dead=true
+    fi
+}
+
+for i in $(seq 1 5); do
+    echo "Attempt $i to terminate watchdog"
+    getWdPid
+    killSafely $wdPid
+    if $dead; then
+        break
+    fi
+    echo "Sleep for 5s"
+    sleep 5s
+done
+
+if ! $dead; then
+    echo "Watchdog cannot be terminated safely, kill it"
+    kill -9 $wdPid
+fi
+
+for i in $(seq 1 5); do
+    echo "Attemt $i to terminate app"
+    getAppPid
+    killSafely $appPid
+    if $dead; then
+        break
+    fi
+    echo "Sleep for 5s"
+    sleep 5s
+done
+
+if ! $dead; then
+    echo "App cannot be terminated safely, kill it"
+    kill -9 $appPid
+fi
+
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,38 +1,4 @@
 #!/bin/bash
 
-getPid() {
-    pid=$(jps | grep HackernewsVkApp | awk '{print $1}')	
-}
-
-startApp() {
-    ./run-java-app.sh
-    ./ping.sh
-}
-
-killApp() {
-    getPid
-    if [ -n "$pid" ]; then
-        echo "Process is running, pid=$pid, try to terminate"
-        kill -15 $pid
-        dead=false
-    else
-        echo "Process is dead"
-        dead=true
-    fi
-}
-
-for i in $(seq 1 15); do
-    echo "Attemt $i to terminate app"
-    killApp
-    if $dead; then
-        startApp
-        exit 0
-    fi
-    echo "Sleep for 5s"
-    sleep 5s
-done
-
-echo "Process cannot be terminated safely, kill it"
-getPid
-kill -9 $pid
-startApp
+./kill.sh
+./run.sh
