e309aa7cf11f7b08b930753e9a3814e88498bd76
dreambrother <nikshmakov@gmail.com>
2015-09-21 22:48:07 +0300

    fix JVM opts

--- scripts/run-java-app.sh
+++ scripts/run-java-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-MEMORY_PARAMS="-Xmx128m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/nik/app/log/ -XX:OnOutOfMemoryError='kill -9 %p'"
+MEMORY_PARAMS="-Xmx128m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/nik/app/log/ -XX:OnOutOfMemoryError=\"kill -9 %p\""
 java $MEMORY_PARAMS -cp "lib/*" com.github.dreambrother.hackernews.HackernewsVkApp >> log/process.log 2>&1 &
